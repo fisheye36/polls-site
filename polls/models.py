809ff4b154969ad1e9ac1ca3b06f748996917e27
Kamil Warchoł <kamil.warchol.93@gmail.com>
2018-08-08 23:29:27 +0200

    Add methods to models
    Add was_published_recently() method to Question model and __str__()
    methods to both models.

--- polls/models.py
+++ polls/models.py
@@ -1,12 +1,24 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     publication_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self, recent_period=datetime.timedelta(days=1.0)):
+        return self.publication_date >= timezone.now() - recent_period
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=100)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
