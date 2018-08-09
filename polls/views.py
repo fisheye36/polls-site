6cc7dbc0c4c00bbcd0226e7836f287528f39ca7e
Kamil Warchoł <kamil.warchol.93@gmail.com>
2018-08-09 22:15:00 +0200

    Modify index view
    Index view now displays 5 latest questions.

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 
+from .models import Question
+
 
 def index(request):
-    return HttpResponse(b'Hello world, you are at the polls app index page.')
+    latest_question_list = Question.objects.order_by('-publication_date')[:5]
+    output = b', '.join(bytes("'%s'" % q.question_text, encoding='utf-8') for q in latest_question_list)
+    return HttpResponse(output)
 
 
 def detail(request, question_id):
