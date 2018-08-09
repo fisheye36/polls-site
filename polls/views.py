eaf08df2442b580d0b245a406fbd0dfd297757e7
Kamil Warchoł <kamil.warchol.93@gmail.com>
2018-08-09 22:54:36 +0200

    Update index view to use Django template
    Instead of hard-coding index page design in view's code Django template
    is used.

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,14 @@
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-publication_date')[:5]
-    output = b', '.join(bytes("'%s'" % q.question_text, encoding='utf-8') for q in latest_question_list)
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = {'latest_question_list': latest_question_list}
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
