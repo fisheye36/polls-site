3faa4534b02ab21c8dfd93b6db5c3fe135e4230d
Kamil Warchoł <kamil.warchol.93@gmail.com>
2018-08-17 23:52:16 +0200

    Disable showing future questions in index view

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.db.models import F
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
 
 from .models import Choice, Question
@@ -12,7 +13,7 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-publication_date')[:5]
+        return Question.objects.filter(publication_date__lte=timezone.now()).order_by('-publication_date')[:5]
 
 
 class DetailView(generic.DetailView):
