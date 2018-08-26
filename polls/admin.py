b2a607027d47524dab2668e31d807b9775c3616b
Kamil Warchoł <kamil.warchol.93@gmail.com>
2018-08-26 23:48:51 +0200

    Register Choice model in admin page

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['publication_date']}),
+    ]
+
+
+admin.site.register(Choice)
+admin.site.register(Question, QuestionAdmin)
